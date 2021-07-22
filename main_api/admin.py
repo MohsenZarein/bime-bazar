6d4191649e7eabb0383e214dcc3c3868d4b79d9e
mohsen zarein <mohsenzarein1379@gmail.com>
2021-07-22 16:50:03 +0430

    registered mina_api models into admin area

--- main_api/admin.py
+++ main_api/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Insurance, Coupon, UserCoupon
 
-# Register your models here.
+@admin.register(Insurance)
+class InsuranceAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+
+@admin.register(Coupon)
+class CouponAdmin(admin.ModelAdmin):
+    list_display = ['id','insurance', 'amount', 'probability']
+    list_select_related = ['insurance',]
+
+
+@admin.register(UserCoupon)
+class UserCouponAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'insurance', 'amount', 'created']
+    list_select_related = ['user', 'insurance',]
